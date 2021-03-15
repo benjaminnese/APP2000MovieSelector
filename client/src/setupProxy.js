87c7222182e75ef73a8a442ba9fa9b5a88322b65
benjamin nese <benjamin.nese@gmail.com>
2021-03-15 17:38:00 +0100

    Forsøk nr 2 fjerne dev.js nøklene

--- client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) { //trengs bare i dev mode, på heroku eksistere ikke create app react
-  app.use(
-    ["/api", "/auth/google/"],
-    createProxyMiddleware({
-      target: "http://127.0.0.1:5000/",
-    })
-  );
-};
