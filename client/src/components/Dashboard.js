141066a08a51176a5abe2335efe3c85c2df455d2
Wavierbear <m.bergers@icloud.com>
2021-03-15 22:32:54 +0100

    Landing til class
    Landing til en class, som skal vise enten Om oss app for å få gjest til å registrere seg. Eller hvis de er en bruker så skal de få frem dashboard

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+// Om oss, component i Landing.js, skal fortelle litt om oss, skal deretter følge en knapp for registrering med google
+
+const Dashboard = () => {
+    return(
+      <div>
+      <h1>Hei</h1>
+      </div>
+    );
+};
+export default Dashboard;
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,21 +1,42 @@
 //Side som bruker kommer til om ikke innlogget
 import background from "./bg-backgrunn.jpg";
 import OmApp from './OmApp'
+import Dashboard from './Dashboard'
+import {connect} from 'react-redux';
 import {Button} from 'react-bootstrap';
 import './App.css';
+import React, { Component } from 'react';
 // Se lucid chart for skjetch for landigpage
 
 
-export const Landing = () =>{
+class Landing extends Component {
+  renderContent(){
+          switch(this.props.auth){
+              case null:
+                  return;
+              case false:
+                return(
+                  <OmApp />);
+              default:
+                  return(
+                     <Dashboard />
+                   );
+          }
+      }
 
+
+  render(){
         return(
             <div className="fixBackgroundParent">
                 <div className="fixBackground"/>
-                    <OmApp/> 
+                {this.renderContent()}
             </div>
-
         );
-
+}
 
 }
-export default Landing;
+function mapStateToProps({auth}) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Landing);
