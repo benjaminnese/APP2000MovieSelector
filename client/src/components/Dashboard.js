7abddaaca0ecf5b3ef17b086beac695de7b7ae30
benjamin nese <benjamin.nese@gmail.com>
2021-03-16 15:29:07 +0100

    Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,43 @@
 import React from 'react';
-
+import {Col, Row, Card, Button, Tab, Tabs} from 'react-bootstrap/';
 
 // Det bruker skal se når de kommer inn på nettside når de er allerede logget it
 
 const Dashboard = () => {
     return(
-      <div className = "position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-        <div className="col-md-5 p-lg-5 mx-auto my-5">
-          <h1>Hei</h1>
-        </div>
-     
-      </div>
+      
+      <Row style={{height:'100vh'}}> {/*Bootstrap deler siden opp i 12 kolonner, sier jeg vil bruke 9 til ene */}
+      
+      <Row className="col-9 justify-content-center">
+        <Col className="col-10 bg-white mt-5"> 
+            <h1 className ="p-5 text-dark" >Hei </h1>
+        </Col>
+    
+        <Col className = "col-5 mt-5">
+        <Card className="text-center">
+          <Card.Header>Featured</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
+        </Col>
+        </Row>
+        <Col className="col-3 pt-3 bg-dark">
+          <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+          <Tab eventKey="home" title="Venner">
+          </Tab>
+          <Tab eventKey="profile" title="Mine sofagrupper">
+          </Tab>
+        
+       </Tabs>
+        </Col>
+      </Row>
+    
     );
 };
 export default Dashboard;
