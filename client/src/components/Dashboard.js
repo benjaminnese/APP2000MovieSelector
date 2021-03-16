cb0e8996ccffe6e9434567d0a2b082fe1fd1a960
benjamin nese <benjamin.nese@gmail.com>
2021-03-16 15:56:13 +0100

    blo

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -9,29 +9,39 @@ const Dashboard = () => {
       <Row style={{height:'100vh'}}> {/*Bootstrap deler siden opp i 12 kolonner, sier jeg vil bruke 9 til ene */}
       
       <Row className="col-9 justify-content-center">
-        <Col className="col-10 bg-white mt-5"> 
-            <h1 className ="p-5 text-dark" >Hei </h1>
+        <Col className="col-10 mt-5 border"> 
+            <h1 className ="p-5 text-white" >Hei (navn på bruker)</h1>
         </Col>
     
         <Col className = "col-5 mt-5">
-        <Card className="text-center">
+        <Card>
+          <Card.Header>Populær</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col className = "col-5 mt-5">
+        <Card>
           <Card.Header>Featured</Card.Header>
           <Card.Body>
             <Card.Title>Special title treatment</Card.Title>
             <Card.Text>
               With supporting text below as a natural lead-in to additional content.
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
           </Card.Body>
-          <Card.Footer className="text-muted">2 days ago</Card.Footer>
         </Card>
         </Col>
         </Row>
-        <Col className="col-3 pt-3 bg-dark">
+    
+        <Col className="col-2 ml-auto pt-3" style={{backgroundColor:'rgba(9, 28, 37,0.9)'}}>
           <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
           <Tab eventKey="home" title="Venner">
           </Tab>
-          <Tab eventKey="profile" title="Mine sofagrupper">
+          <Tab eventKey="profile" title="Innstillinger">          
           </Tab>
         
        </Tabs>
